13c1f0ea70201bb032c30f220a2402e71f3e0791
Eemi <eemi.korhonen@metropolia.fi>
2024-04-04 12:59:29 +0300

    fixes

--- database/inc/header.php
+++ database/inc/header.php
@@ -25,7 +25,17 @@ endif;
         <h1>Media Library</h1>
         <nav>
             <ul>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="index.php">Login</a></li>
+                <?php
+                if (isset($_SESSION['user'])):
+                    ?>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                <?php
+                else:
+                    ?>
+                    <li><a href="index.php">Login</a></li>
+                <?php
+                endif;
+                ?>
             </ul>
     </header>
--- database/register.php
+++ database/register.php
@@ -1,29 +1,27 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 global $DBH;
-require_once 'dbConnect.php';
+require 'dbConnect.php';
 
-$sql = 'SELECT * FROM MediaItems;';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
+        $data = [
+            'username' => $_POST['username'],
+            'password' => password_hash($_POST['password'], PASSWORD_BCRYPT),
+            'email' => $_POST['email']
+        ];
 
-try {
-    $STH = $DBH->query($sql);
-    $STH->setFetchMode(PDO::FETCH_ASSOC);
-    while ($row = $STH->fetch()) {
-        echo '<tr>';
-        echo '<td>' . $row['media_id'] . '</td>';
-        echo '<td>' . $row['user_id'] . '</td>';
-        echo '<td><img alt="kuva" src="uploads/' . $row['filename'] . '"></td>';
-        echo '<td>' . $row['filesize'] . '</td>';
-        echo '<td>' . $row['media_type'] . '</td>';
-        echo '<td>' . $row['title'] . '</td>';
-        echo '<td>' . $row['description'] . '</td>';
-        echo '<td>' . $row['created_at'] . '</td>';
-        echo '<td>
-                <a href="deleteData.php?id=' . $row['media_id'] . '">Delete</a>
-                <a href="#" class="modify-link" data-id="' . $row['media_id'] . '">Modify</a>   
-              </td>';
-        echo '</tr>';
+        $sql = 'INSERT INTO Users (username, password, email, user_level_id) VALUES (:username, :password, :email, 2)';
+
+        try {
+            $STH = $DBH->prepare($sql);
+            $STH->execute($data);
+            header('Location: index.php?success=Registration successful');
+        } catch (PDOException $e) {
+            echo "Could not insert data into the database.";
+            file_put_contents('PDOErrors.txt', 'register.php - ' . $e->getMessage(), FILE_APPEND);
+        }
     }
-} catch (PDOException $e) {
-    echo "Could not select data from the database.";
-    file_put_contents('PDOErrors.txt', 'selectData.php - ' . $e->getMessage(), FILE_APPEND);
 }
+?>
